d3e659be79e7c8064f249b4bb97a0f3388ce36ea
Lance Ballicud <lmuballicud@gmail.com>
2025-07-22 17:00:23 +0800

    setup firebase for backend, create auth context and utilize auth from firebase for authentication

---
+++ src/firebase/auth.js
@@ -0,0 +1,41 @@
+import { createUserWithEmailAndPassword, GoogleAuthProvider, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, updatePassword } from "firebase/auth";
+import { auth } from "./firebase";
+
+export const doCreateUserWithEmailAndPassword = async (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const doSignInWithEmailAndPassword = async (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+}
+
+export const doSignOut = async () => {
+    return auth.signOut();
+}
+
+// Extras
+
+export const doPasswordReset = async (email) => {
+    return sendPasswordResetEmail(auth, email);
+}
+
+export const doPasswordChange = async (password) => {
+    return updatePassword(auth.currentUser, password);
+}
+
+export const doSendEmailVerification = async () => {
+    return sendEmailVerification(auth.currentUser, {
+        url: `${window.location.origin}/home`
+    })
+}
+
+
+// For Google Sign-In.
+// export const doSignInWithGoogle = async () => {
+//     const provider = new GoogleAuthProvider();
+//     const result = await signInWithPopup(auth, provider);
+
+
+
+//     return result;
+// }
